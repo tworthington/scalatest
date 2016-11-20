3ba2aa96fb8f41e6fd4b047a8e4b22984f6a220e
Thomas Worthington <thomas@technouveau.co.uk>
2016-11-20 16:29:51 +0000

    Fix div by zero if no discount

--- src/main/scala/fruit.scala
+++ src/main/scala/fruit.scala
@@ -3,5 +3,5 @@ package com.beis.application
 // offer is # needed to get free item
 class Fruit(val name: String, val price: Double, val offer: Int =0)
 {
-  def discountFor(n:Int):(Double) = (n/offer)*price
+  def discountFor(n:Int):(Double) = if(offer>0) ((n/offer)*price) else 0
 }
