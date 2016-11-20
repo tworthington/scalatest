1a2707d1c0f0fc12bdec2a03c9564a2e0acea99b
Thomas Worthington <thomas@technouveau.co.uk>
2016-11-20 15:05:39 +0000

    Basic shop tests

--- src/test/scala/beisTests.scala
+++ src/test/scala/beisTests.scala
@@ -11,4 +11,18 @@ class BeisTests extends FunSuite with BeforeAndAfter
     shop=new Store(Array(new Fruit("Apple",.60), new Fruit("Orange",.25)))
   }
 
+  test("Empty string is 0p")
+  {
+    assert(shop.bill("")===0)
+  }
+
+  test("Single Apple is 60p")
+  {
+    assert(shop.bill("Apple")===.6)
+  }
+
+  test("Single Orange is 25p")
+  {
+    assert(shop.bill("Orange")===.25)
+  }
 }
